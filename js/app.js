ff8c779ebf2f59c8f8f371b07eba756a149be841
Tymofii Yuzefovych <tymofii.yuzefovych@gmail.com>
2017-03-11 10:51:27 +0100

    update todolist

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var removeFinishedTasksButton = document.getElementById("removeFinishedTasksButton");
     var span = document.getElementById("counter");
     var counter = 0;
-
+    span.innerText = counter;
+    
     addTaskButton.addEventListener("click", function() {
         var valueInput = taskInput.value;
         if (validateInput(valueInput) === false) {
@@ -17,18 +18,26 @@ document.addEventListener('DOMContentLoaded', function() {
         var button = document.createElement("button");
         var complete = document.createElement("button");
 
+
         taskList.appendChild(li);
         counter++;
         span.innerText = counter;
         li.appendChild(h1);
-        var bgh1 = h1.appendChild(node);
+        var btnh1 = h1.appendChild(node);
         var buttonDelete = li.appendChild(button);
         var buttonComplete = li.appendChild(complete);
+        buttonComplete.setAttribute("id", "complete2");
+        buttonDelete.setAttribute("id", "delete2");
+
+
+
         button.innerText = "Delete";
         complete.innerText = "Complete";
-
+        
         buttonComplete.addEventListener("click", function() {
-            bgh1.parentNode.parentNode.classList.add("complete")
+            btnh1.parentNode.parentNode.classList.toggle("complete");
+            
+
         })
 
         buttonDelete.addEventListener("click", function() {
@@ -41,24 +50,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
     removeFinishedTasksButton.addEventListener("click", function() {
         var liComplete = document.querySelectorAll(".complete");
+        if(counter==0){
+                return null;
+            }else{
         for (var i = 0; i < liComplete.length; i++) {
-            if (liComplete[i].className === "complete") {
+            if (liComplete[i].className == "complete") {
                 liComplete[i].parentNode.removeChild(liComplete[i]);
             }
+            
+        }
         }
         counter--;
         span.innerText = counter;
     });
 
     function validateInput(valueInput) {
-        if (valueInput.length < 5 || valueInput.length > 100) {
+        if (valueInput.length < 5 || valueInput.length > 400) {
             return false;
         } else {
             return true;
         }
-
-
-
     };
 
 });
